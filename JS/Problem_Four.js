f58fce46ee25df1a7e8d5e65dc5b901a3c59196f
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2022-10-03 11:25:29 +0600

    Problem four solved

---
+++ JS/Problem_Four.js
@@ -0,0 +1,11 @@
+/*
+ Sarah’s mother is teaching her mathematics. She gave Sarah a number and asked her what the
+ remainder would be if she divided the number by 5. Help Sarah write the program.
+*/
+
+let givenNumber = 119;
+let dividerNumber = 5;
+
+let getRemainder = givenNumber % dividerNumber;
+
+console.log(getRemainder);
