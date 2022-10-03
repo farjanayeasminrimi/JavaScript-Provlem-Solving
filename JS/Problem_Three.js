10c871dd5a29d64f648572037dfe90750b0d6882
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2022-10-03 11:20:06 +0600

    Problem three solved

---
+++ JS/Problem_Three.js
@@ -0,0 +1,11 @@
+/*
+ John’s teacher gave him two variables. Each variable contains a string. John’s teacher asked him to
+ combine these two strings and print them in one line. Help John write the program.
+*/
+
+let firstString = 'I am a pro';
+let lastString = 'Programmer';
+
+let combineStrings = firstString +' '+ lastString;
+
+console.log(combineStrings);
