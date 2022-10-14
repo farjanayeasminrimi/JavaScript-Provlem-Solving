105e6245dbfa1bc06502e175b605a3b47be6fdee
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2022-10-14 20:02:46 +0600

    Problem fifteen solved

---
+++ JS/Problem_Fifteen.js
@@ -0,0 +1,9 @@
+/*
+ Write a function which will take a value in uppercase and will return the output of the value in lowercase.
+*/
+function takeName(eachName){
+  let returnValue = eachName.toLowerCase();
+  return returnValue;
+}
+let getValue = takeName('Umme Saha');
+console.log(getValue);
