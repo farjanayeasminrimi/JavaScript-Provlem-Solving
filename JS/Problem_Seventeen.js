25bf425e26e33956fe67012d674049f3483b30d9
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2022-10-14 23:22:14 +0600

    Problem seventeen solved

---
+++ JS/Problem_Seventeen.js
@@ -0,0 +1,9 @@
+/*
+ Write a function which will take a number as an argument and will return number's square.
+*/
+function getSquare(number){
+ let doSquare = number * number;
+ return doSquare;
+}
+const squareValue = getSquare(10);
+console.log(squareValue);
