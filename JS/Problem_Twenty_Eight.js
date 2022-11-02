e26d59b59c8bee3cff350cc81f1ab5ca128cabd3
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2022-11-02 22:46:55 +0600

    Problem Twenty Nine solved

--- JS/Problem_Twenty_Eight.js
+++ JS/Problem_Twenty_Eight.js
@@ -2,15 +2,15 @@
  Write a function to get the highest number in an array.
 */
 function maxInArray(numbers){
- let first_height = numbers[0];
+ let highestNum = numbers[0];
  for(let i = 0; i < numbers.length; i++){
   const index = i;
   const getElement = numbers[index];
-  if(getElement > first_height){
-    first_height = getElement;
+  if(getElement > highestNum){
+    highestNum = getElement;
   }
  }
- return first_height;
+ return highestNum;
 }
 const heights = [299, 294, 322, 923]; 
 const getAns = maxInArray(heights);
--- JS/Problem_Twenty_Nine.js
+++ JS/Problem_Twenty_Nine.js
@@ -1,1 +1,17 @@
-// write a function to get the lowest number in an array
+/*
+ Write a function to get the lowest number in an array.
+*/
+function minInArray(numbers){
+  let lowestNum = numbers[0];
+  for(let i = 0; i < numbers.length; i++){
+   const index = i;
+   const getElement = numbers[index];
+   if(getElement < lowestNum){
+     lowestNum = getElement;
+   }
+  }
+  return lowestNum;
+ }
+ const heights = [299, 84, 322, 923]; 
+ const getAns = minInArray(heights);
+ console.log(getAns);
